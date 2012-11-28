e5e19d2de5a42934b846f3aca2662850f989e3f3
arjankranenburg <Arjan.Kranenburg@gmail.com>
2012-11-28 23:06:51 +0100

    Changed display name, since the comment was displayed already

--- src/main/java/net/sf/testium/executor/general/Comment.java
+++ src/main/java/net/sf/testium/executor/general/Comment.java
@@ -52,6 +52,6 @@ public class Comment extends GenericCommandExecutor {
 		{
 			System.out.println( comment );
 		}
-		result.setDisplayName( this.toString() + "\"" + comment + "\"" );
+//		result.setDisplayName( this.toString() + "\"" + comment + "\"" );
 	}
 }
