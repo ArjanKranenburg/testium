499447ea34f737c907d1d555e697facc0fc368c9
arjankranenburg' <Arjan.Kranenburg@gmail.com>
2013-03-06 20:53:38 +0100

    Fix issue #23: Adding lessThan and greaterThan to checkListSize. exact remains default.

--- src/main/java/net/sf/testium/executor/general/CheckListSize.java
+++ src/main/java/net/sf/testium/executor/general/CheckListSize.java
@@ -9,17 +9,22 @@ import org.testtoolinterfaces.testresult.TestStepResult;
 import org.testtoolinterfaces.testsuite.ParameterArrayList;
 import org.testtoolinterfaces.testsuite.TestSuiteException;
 import org.testtoolinterfaces.utils.RunTimeData;
+
 public class CheckListSize extends GenericCommandExecutor
 {
 	private static final String COMMAND = "checkListSize";
 
 	private static final String PAR_LIST = "list";
 	private static final String PAR_SIZE = "size";
+	private static final String PAR_MATCH = "match";
 
 	private static final SpecifiedParameter PARSPEC_LIST = new SpecifiedParameter( 
 			PAR_LIST, List.class, false, false, true, false );
 	private static final SpecifiedParameter PARSPEC_SIZE = new SpecifiedParameter( 
 			PAR_SIZE, Integer.class, false, true, true, false );
+	private static final SpecifiedParameter PARSPEC_MATCH = new SpecifiedParameter( 
+			PAR_MATCH, String.class, true, true, true, false )
+			.setDefaultValue("exact");
 
 	/**
 	 *
@@ -30,6 +35,7 @@ public class CheckListSize extends GenericCommandExecutor
 
 		this.addParamSpec( PARSPEC_LIST );
 		this.addParamSpec( PARSPEC_SIZE );
+		this.addParamSpec( PARSPEC_MATCH );
 	}
 
 	@Override
@@ -40,13 +46,62 @@ public class CheckListSize extends GenericCommandExecutor
 		@SuppressWarnings("unchecked")
 		List<Object> list = (List<Object>) this.obtainValue(aVariables, parameters, PARSPEC_LIST);
 		int expectedSize = (Integer) this.obtainValue(aVariables, parameters, PARSPEC_SIZE);
+		String match = (String) this.obtainOptionalValue(aVariables, parameters, PARSPEC_MATCH);
 
 		String listName = parameters.get(PAR_LIST).getName();
 		result.setDisplayName( this.toString() + " " + listName + " " + expectedSize );
 
-		if ( list.size() != expectedSize )
+		
+		
+		if ( match.equalsIgnoreCase( "exact" ) )
 		{
-			throw new TestSuiteException( "List size was " + list.size() + ". Expected " + expectedSize );
+			checkExact(list, expectedSize,
+					"List size was " + list.size() + ". Expected " + expectedSize );
+			return;
 		}
+		else if ( match.equalsIgnoreCase( "lessThan" ) )
+		{
+			checkLessThan(list, expectedSize,
+					"List size was " + list.size() + ". Expected " + expectedSize );
+			return;
+		}
+		else if ( match.equalsIgnoreCase( "greaterThan" ) )
+		{
+			checkGreaterThan(list, expectedSize,
+					"List size was " + list.size() + ". Expected " + expectedSize );
+			return;
+		}
+		else
+		{
+			throw new Exception( "match criteria \"" + match + "\" is not supported. Only exact, lessThan, or greaterThan" );
+		}
+
+	}
+
+	/**
+	 * @param list
+	 * @param expectedSize
+	 * @param message
+	 * @throws TestSuiteException
+	 */
+	private void checkExact(List<Object> list, int expectedSize, String message)
+			throws TestSuiteException {
+		if ( list.size() != expectedSize ) {
+			throw new TestSuiteException( message );
+		}
+	}
+
+	private void checkLessThan(List<Object> list, int referenceSize, String message)
+			throws TestSuiteException {
+		if ( list.size() < referenceSize ) {
+			throw new TestSuiteException( message );
+		}
+	}
+
+	private void checkGreaterThan(List<Object> list, int referenceSize, String message)
+		throws TestSuiteException {
+	if ( list.size() > referenceSize ) {
+		throw new TestSuiteException( message );
+	}
 	}
 }
